78dac6e036ac3f4ba2b536aa22da2b87d8a019b8
smartajay2 <ajayjest2@gmail.com>
2024-04-08 12:13:03 +0400

    newly updated

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,9 +1,11 @@
-function Todoitem(props) {
+function Todoitem(props)
+ {
   const Activityarr = props.activityarr
   const setactivity = props.setactivity
 
      function Handledelete(deleteid) {
-       var temp = Activityarr.filter(function (item) {
+       var temp =  Activityarr.filter(function
+         (item) {
         if (item.id === deleteid) 
         {
          return false
@@ -21,10 +23,11 @@ function Todoitem(props) {
   return (
       <div>
         <p>{props.index + 1}.{props.activity}</p>
-        <button className="text-red-500" onClick={() => Handledelete(props.id)}>Delete</button>
+        <button className="text-red-500" onClick={() =>{ Handledelete(props.id)}}>Delete</button>
       </div>
 
     )
   }
 
   export default Todoitem
+
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react"
 import Todoitem from "./Todoitem"
 
-export default function Todolist() {
+ function Todolist() {
   const [Activityarr, setactivity] = useState([
-    { id: 1, activity: "Go for a walk" }
-    , { id: 1, activity: "Go to office" }
-    , { id: 1, activity: "Go to office" }
+
+    { id: 1, 
+      activity: "Go for a walk" }
+    ,
+     { id: 1,
+       activity: "Go to office" }
+    , 
+    { id: 1,
+       activity: "Go to office" }
 
   ])
 
   return (
     <div className="bg-[#BDB4EA] border rounded-md p-2 flex-grow">
       <h1 className="text-2xl font-medium">Today's Activity</h1>
-      {/* <p>{activityarr[0].activity}</p>
-          <p>{activityarr[1].activity}</p> */}
+      
 
       {
         Activityarr.map(function (item, index) {
@@ -25,3 +30,5 @@ export default function Todolist() {
     </div>
   )
 }
+
+export default Todolist
